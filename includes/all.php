7c37ee97585ff4a32e17c7ca1e3ea1a7ed233472
zraziu <zraziu@mail.ru>
2018-02-05 22:16:20 +0300

    список в div

--- includes/all.php
+++ includes/all.php
@@ -5,52 +5,48 @@
 
 <h1>Настройка цен</h1>
 <br>
-<h3>Экскурсии / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h3>
-<ul class="exc-list">
+<h2 class="all-h2">Экскурсии / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h2>
+<div class="exc-list">
     <?
     $vlg = vlg_cat('excursion');
-    // выводим по одной записи
     foreach ($vlg as $op): ?>
-    <li class="exc-item"><?=$op['name']?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></li>
+    <div class="exc-item"><?=$op['name']?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></div>
     <? endforeach; ?>
-</ul>
+</div>
 <br>
 
-<h3>Музеи / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add-musem">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h3>
-<ul class="exc-list">
+<h2 class="all-h2">Музеи / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add-musem">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h2>
+<div class="exc-list">
     <?
     $vlg = vlg_cat('musem');
-    // выводим по одной записи
     foreach ($vlg as $op): ?>
-        <li class="exc-item"><?=$op['name']?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></li>
+        <div class="exc-item"><?=$op['name']?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></div>
     <? endforeach; ?>
-</ul>
+</div>
 
 <br>
 <br>
-<h3>Гостиницы / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add-hotel">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h3>
-<ul class="exc-list">
+<h2 class="all-h2">Гостиницы / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add-hotel">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h2>
+<div class="exc-list">
     <?
     $vlg = vlg_cat('hotel');
-    // выводим по одной записи
     foreach ($vlg as $op): ?>
-        <li class="exc-item"><?=$op['name']?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></li>
+        <div class="exc-item"><?=$op['name']?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></div>
     <? endforeach; ?>
-</ul>
+</div>
 
 <br>
 <br>
-<h3>Услуги / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add-services">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h3>
-<ul class="exc-list">
+<h2 class="all-h2">Услуги / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add-services">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h2>
+<div class="exc-list">
     <?
     $vlg = vlg_cat('service');
-    // выводим по одной записи
     foreach ($vlg as $op): ?>
-        <li class="exc-item"><?=$op['name']?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></li>
+        <div class="exc-item"><?=$op['name']?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></div>
     <? endforeach; ?>
-</ul>
+</div>
 
 <br>
 <br>
-<h3>Комиссия</h3>
+<h2 class="all-h2">Комиссия</h2>
 <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit-commission&id=1"><i class="icon-sliders"></i> Редактировать комиссию</a>
