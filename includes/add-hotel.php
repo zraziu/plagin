ddda78ed9aa3f6444cc83f470e590ffb2d7db693
zraziu <zraziu@mail.ru>
2018-02-05 14:08:22 +0300

    перенес/удалил скрипт загрузки фото

--- includes/add-hotel.php
+++ includes/add-hotel.php
@@ -6,8 +6,6 @@ include_once('m/vlg.php');
 
 <?php
 
-
-
 if (!empty($_POST)) {
     // все ли поля заполнены
     if(vlg_add($_POST['title'], $_POST['description'], $_POST['pricePerPerson'], $_POST['prise0'], $_POST['prise16'], $_POST['prise18'], $_POST['prise'], $_POST['hours'], $_POST['category'])) {
@@ -27,40 +25,6 @@ if (!empty($_POST)) {
     }
 } else {
     $error = false;
-}
-/**
- * Image Uploader
- * author: Arthur Gareginyan www.arthurgareginyan.com
- */
-function arthur_image_uploader( $name, $width, $height ) {
-    wp_enqueue_media();
-
-    // Set variables
-    $options = get_option( 'RssFeedIcon_settings' );
-    $default_image = plugins_url('img/logo.jpg', __FILE__);
-
-    if ( !empty( $options[$name] ) ) {
-        $image_attributes = wp_get_attachment_image_src( $options[$name], array( $width, $height ) );
-        $src = $image_attributes[0];
-        $value = $options[$name];
-    } else {
-        $src = $default_image;
-        $value = '';
-    }
-
-    $text = __( 'Upload', RSSFI_TEXT );
-
-    // Print HTML field
-    echo '
-        <div class="upload">
-            <img data-src="' . $default_image . '" src="' . $src . '" width="' . $width . 'px" height="' . $height . 'px" />
-            <div>
-                <input type="hidden" name="RssFeedIcon_settings[' . $name . ']" id="RssFeedIcon_settings[' . $name . ']" value="' . $value . '" />
-                <button type="submit" class="upload_image_button button">' . $text . '</button>
-                <button type="submit" class="remove_image_button button">&times;</button>
-            </div>
-        </div>
-    ';
 }
 
 ?>
@@ -131,28 +95,4 @@ function arthur_image_uploader( $name, $width, $height ) {
             }
         }
     }
-
-    // The "Upload" button
-    $('.upload_image_button').click(function() {
-        var send_attachment_bkp = wp.media.editor.send.attachment;
-        var button = $(this);
-        wp.media.editor.send.attachment = function(props, attachment) {
-            $(button).parent().prev().attr('src', attachment.url);
-            $(button).prev().val(attachment.id);
-            wp.media.editor.send.attachment = send_attachment_bkp;
-        }
-        wp.media.editor.open(button);
-        return false;
-    });
-
-    // The "Remove" button (remove the value from input type='hidden')
-    $('.remove_image_button').click(function() {
-        var answer = confirm('Are you sure?');
-        if (answer == true) {
-            var src = $(this).parent().prev().attr('data-src');
-            $(this).parent().prev().attr('src', src);
-            $(this).prev().prev().val('');
-        }
-        return false;
-    });
 </script>
