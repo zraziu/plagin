a6d6a9622ff7fb09ec587645b651e621879e3ece
zraziu <zraziu@mail.ru>
2018-02-05 14:55:27 +0300

    Перевел предупреждение

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,3 +1,19 @@
+// Показать - скрыть блок цен
+var elems = document.getElementsByName('pricePerPerson'); //массив элементов
+document.addEventListener("DOMContentLoaded", ready); // стр загрузилась, выполняется функия ready
+
+function ready() {
+    for(var i = 0; i < elems.length; i++) {
+        if (elems[i].checked) {
+            document.getElementById('Block1').style.display='none';
+            document.getElementById('Block2').style.display='none';
+            document.getElementById('Block3').style.display='none';
+
+            document.getElementById('Block'+(i+1)).style.display='block';
+        }
+    }
+}
+
 // Кнопка загрузки изображений - "Upload" button
 $('.upload_image_button').click(function() {
     var send_attachment_bkp = wp.media.editor.send.attachment;
@@ -13,7 +29,7 @@ $('.upload_image_button').click(function() {
 
 // Кнопка удалить - The "Remove" button (remove the value from input type='hidden')
 $('.remove_image_button').click(function() {
-    var answer = confirm('Are you sure?');
+    var answer = confirm('Вы уверены?');
     if (answer == true) {
         var src = $(this).parent().prev().attr('data-src');
         $(this).parent().prev().attr('src', src);
