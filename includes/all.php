31d7c3d5192b9a5093ca5f2c031ac5a05c7321d3
zraziu <zraziu@mail.ru>
2018-03-03 09:35:16 +0300

    переименовал классы стилей

--- includes/all.php
+++ includes/all.php
@@ -5,42 +5,42 @@
 
 <h1>Настройка цен</h1>
 <br>
-<h2 class="all-h2">Экскурсии / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h2>
-<div class="exc-list">
+<h2 class="vlg-a__h2">Экскурсии / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h2>
+<div class="vlg-a__list">
     <?
     $vlg = vlg_cat('excursion');
     foreach ($vlg as $op): ?>
-    <div class="exc-item"><?=wp_unslash($op['name'])?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></div>
+    <div class="vlg-a__item"><?=wp_unslash($op['name'])?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></div>
     <? endforeach; ?>
 </div>
 <br><hr>
-<h2 class="all-h2">Музеи / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add-musem">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h2>
-<div class="exc-list">
+<h2 class="vlg-a__h2">Музеи / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add-musem">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h2>
+<div class="vlg-a__list">
     <?
     $vlg = vlg_cat('musem');
     foreach ($vlg as $op): ?>
-        <div class="exc-item"><?=wp_unslash($op['name'])?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></div>
+        <div class="vlg-a__item"><?=wp_unslash($op['name'])?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></div>
     <? endforeach; ?>
 </div>
 <br><hr>
-<h2 class="all-h2">Гостиницы / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add-hotel">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h2>
-<div class="exc-list">
+<h2 class="vlg-a__h2">Гостиницы / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add-hotel">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h2>
+<div class="vlg-a__list">
     <?
     $vlg = vlg_cat('hotel');
     foreach ($vlg as $op): ?>
-        <div class="exc-item"><?=wp_unslash($op['name'])?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></div>
+        <div class="vlg-a__item"><?=wp_unslash($op['name'])?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></div>
     <? endforeach; ?>
 </div>
 <br><hr>
-<h2 class="all-h2">Услуги / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add-services">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h2>
-<div class="exc-list">
-    <div class="exc-item">Трансфер <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit-transfer&id=2"><i class="fa fa-pencil"></i> изменить</a></div>
+<h2 class="vlg-a__h2">Услуги / <a  href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=add-services">Добавить <i class="fa fa-plus-circle fa-green"></i></a></h2>
+<div class="vlg-a__list">
+    <div class="vlg-a__item">Трансфер <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit-transfer&id=2"><i class="fa fa-pencil"></i> изменить</a></div>
     <?
     $vlg = vlg_cat('service');
     foreach ($vlg as $op): ?>
-        <div class="exc-item"><?=wp_unslash($op['name'])?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></div>
+        <div class="vlg-a__item"><?=wp_unslash($op['name'])?> <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit&id=<?=$op['id']?>"><i class="fa fa-pencil"></i> изменить</a></div>
     <? endforeach; ?>
 </div>
 <br><hr>
-<h2 class="all-h2">Комиссия</h2>
+<h2 class="vlg-a__h2">Комиссия</h2>
 <a href="<?=$_SERVER["PHP_SELF"]?>?page=vlg&c=edit-commission&id=1"><i class="icon-sliders"></i> Редактировать комиссию</a>
--- includes/intro.php
+++ includes/intro.php
@@ -5,21 +5,52 @@
 
 ?>
 
-
-<h3>Выберите экскурсии, которые вы хотите посетить</h3>
-<div class="exc-list">
-    <?
-    $vlg = vlg_cat('excursion');
-    foreach ($vlg as $op): ?>
-    <div class="exc-item"><?=$op['name']?> - </div>
-    <? endforeach; ?>
+<h1>Калькулятор</h1>
+<h2>Спланировать тур / Основные данные</h2>
+<div class="vlg-one">
+    <div class="vlg-one__day">Количество дней </div>
+    
+    <div class="vlg-one__people"><button class="" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-users fa-icon-btn"></i> Количество человек: <span class="btnDropdownPeople"></span> <span class="btnDropdownPeopleFree"></span></button>
+        <div class="dropdown-menu">
+            <div class="dropdown-item"><div class="dropdown-item-people">Дети до 15 лет</div><div class="number"><span class="quont-minus"><i class="fa fa-minus" aria-hidden="true"></i></span><input id="inputPeople16" class="input-calc-exc" type="text" min="0" max="100" value="40"><span class="quont-plus"><i class="fa fa-plus" aria-hidden="true"></i></span>
+                </div>
+            </div>
+            <div class="dropdown-item"><div class="dropdown-item-people">Дети до 18 лет</div><div class="number"><span class="quont-minus"><i class="fa fa-minus" aria-hidden="true"></i></span><input id="inputPeople18" class="input-calc-exc" type="text" min="0" max="100" value="0"><span class="quont-plus"><i class="fa fa-plus" aria-hidden="true"></i></span></div>
+            </div>
+            <div class="dropdown-item"><div class="dropdown-item-people">Взрослых</div><div class="number"><span class="quont-minus"><i class="fa fa-minus" aria-hidden="true"></i></span><input id="inputPeople" class="input-calc-exc" type="text" min="0" max="100" value="0"><span class="quont-plus"><i class="fa fa-plus" aria-hidden="true"></i></span></div>
+            </div>
+            <div class="dropdown-divider"></div>
+            <div class="dropdown-item"><div class="dropdown-item-people">Бесплатно</div><div class="number"><span class="quont-minus"><i class="fa fa-minus" aria-hidden="true"></i></span><input id="inputPeopleFree"  class="input-calc-exc" type="text" min="0" max="10" value="4"><span class="quont-plus"><i class="fa fa-plus" aria-hidden="true"></i></span>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
-<h3>Выберите места, которые Вы хотели бы посетить</h3>
-<div class="exc-list">
-    <?
-    $vlg = vlg_cat('musem');
-    foreach ($vlg as $op): ?>
-        <div class="exc-item"><?=$op['name']?></div>
-    <? endforeach; ?>
+
+
+
+
+
+<h2>Места, которые Вы хотите посетить</h2>
+<div  class="vlg-two">
+    <h3>Выберите экскурсии, которые вы хотите посетить</h3>
+    <div class="exc-list">
+        <?
+        $vlg = vlg_cat('excursion');
+        foreach ($vlg as $op): ?>
+            <div class="exc-item"><?=$op['name']?> - </div>
+        <? endforeach; ?>
+    </div>
+
+    <h3>Выберите места, которые Вы хотели бы посетить</h3>
+    <div class="exc-list">
+        <?
+        $vlg = vlg_cat('musem');
+        foreach ($vlg as $op): ?>
+            <div class="exc-item"><?=$op['name']?></div>
+        <? endforeach; ?>
+    </div>
 </div>
+
+<h2>Условия проживания</h2>
