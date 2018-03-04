90a54dd66e64165cb8a0fba78a963188db72f0fa
zraziu <zraziu@mail.ru>
2018-03-04 22:09:34 +0300

    модальное окно + табы

--- includes/intro.php
+++ includes/intro.php
@@ -30,7 +30,7 @@ echo '<link rel="stylesheet" type="text/css" href="'.plugins_url("vlg_plagin/inc
     </div>
     <div class="col">
         <div class="btn-group btn-group-margin">
-            <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-info fa-icon-btn" aria-hidden="true"></i> Дней: <span class="btnDropdownDay">1</span></button>
+            <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-calendar fa-icon-btn" aria-hidden="true"></i> Дней: <span class="btnDropdownDay">1</span></button>
             <div class="dropdown-menu">
                 <div class="dropdown-item"><div class="dropdown-item-people"> </div>
                     <div class="number dropdown-item-number"><span class="quont-minus"><i class="fa fa-minus" aria-hidden="true"></i></span><input id="inputDays" class="input-calc-exc" type="text" min="1" max="5" value="1"><span class="quont-plus"><i class="fa fa-plus" aria-hidden="true"></i></span>
@@ -39,7 +39,21 @@ echo '<link rel="stylesheet" type="text/css" href="'.plugins_url("vlg_plagin/inc
             </div>
         </div>
     </div>
-
+    <div class="col">
+        <div class="btn-group btn-group-margin">
+            <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-bus fa-icon-btn" aria-hidden="true"></i> Трансфер: <span class="btnDropdownBus">0</span></button>
+            <div class="dropdown-menu">
+                <div class="dropdown-item">
+                    <label class="tgl" style="font-size:22px"><input type="checkbox" class="bus-to-or-no" checked /><span data-on="Туроператора" data-off="&nbsp;&nbsp;&nbsp;Заказчика&nbsp;&nbsp;&nbsp;"></span></label>
+                </div>
+                <div class="block-input-bus">
+                    <div class="dropdown-item"><div class="dropdown-item-people">Часов</div><div class="number dropdown-item-number"><span class="quont-minus"><i class="fa fa-minus" aria-hidden="true"></i></span><input id="inputBus" class="inputBus input-calc-exc" type="text" min="0" max="100" value="0"><span class="quont-plus"><i class="fa fa-plus" aria-hidden="true"></i></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <!-- btnDropdownDay #inputDays  vlg-two__delete vlg-two__move -->
@@ -60,32 +74,62 @@ echo '<link rel="stylesheet" type="text/css" href="'.plugins_url("vlg_plagin/inc
             <div class="vlg-two__time">0 ч 40 м</div>
             <div class="vlg-two__bus">Автобус</div>
         </div>
-        <div class="vlg-two_title">Обзорная экскурсия «Город-герой Волгоград» <div><i class="fa fa-trash fa-red vlg-two__delete"></i> <i class="fa fa-arrows-v vlg-two__move"></i></div></div>
+        <div class="vlg-two_title">Обзорная экскурсия «Город-герой Волгоград» <div class="vlg-two__btn"><i class="fa fa-trash fa-red vlg-two__delete"></i> <i class="fa fa-arrows-v vlg-two__move"></i></div></div>
+    </div>
+    <div class="vlg-two__add">
+        <i class="fa fa-plus fa-lg fa-green"></i> Добавить экскурсию
     </div>
 </div>
 
-<h3>Выберите экскурсии, которые вы хотите посетить</h3>
-<div class="exc-list">
-    <?
-    $vlg = vlg_cat('excursion');
-    foreach ($vlg as $op): ?>
-        <div class="exc-item"><?=$op['name']?> - </div>
-    <? endforeach; ?>
-</div>
-
-<h3>Выберите места, которые Вы хотели бы посетить</h3>
-<div class="exc-list">
-    <?
-    $vlg = vlg_cat('musem');
-    foreach ($vlg as $op): ?>
-        <div class="exc-item"><?=$op['name']?></div>
-    <? endforeach; ?>
-</div>
-
-
 <h2 class="excursion-switch-title">ШАГ 3: Условия проживания</h2>
 <div class="vlg-three">
-
+    <div>Отель</div>
 </div>
 
-<h2 class="excursion-switch-title">ШАГ 4: Условия проживания</h2>
+<h2 class="excursion-switch-title">ШАГ 4: Бронирование</h2>
+<div class="vlg-four">
+    <button type="button" class="btn btn-primary excursion-support-btn linkButton" data-toggle="modal" data-target=".modal-sm-help" title="">Задать</button>
+</div>
+
+
+
+
+<!-- Модальное Экскурсии -->
+<div id="vlgExcModal" class="modal fade modal-sm-help" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+            <!-- Форма заявки -->
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Выберите экскурсии</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Выберите места</a>
+                </li>
+            </ul>
+
+            <div class="tab-content vlg-modal" id="myTabContent">
+                    <div class="tab-pane fade in active show" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        
+                            <?
+                            $vlg = vlg_cat('excursion');
+                            foreach ($vlg as $op): ?>
+                                <div class="exc-item"><?=$op['name']?> - </div>
+                            <? endforeach; ?>
+                    </div>
+                    <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                            <?
+                            $vlg = vlg_cat('musem');
+                            foreach ($vlg as $op): ?>
+                                <div class="exc-item"><?=$op['name']?></div>
+                            <? endforeach; ?>
+                    </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
