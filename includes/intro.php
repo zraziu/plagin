e68caf697cbe94ecf3114e04f571e79209162b2e
zraziu <zraziu@mail.ru>
2018-03-04 09:43:17 +0300

    добавлены кнопки удалить, переместить

--- includes/intro.php
+++ includes/intro.php
@@ -11,7 +11,7 @@ echo '<link rel="stylesheet" type="text/css" href="'.plugins_url("vlg_plagin/inc
 <h2 class="excursion-switch-title">ШАГ 1: Спланировать тур / Основные данные</h2>
 
 <div class="row d-flex justify-content-between">
-    <div class="col" style="text-align: center;">
+    <div class="col">
         <div class="btn-group btn-group-margin"><button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-users fa-icon-btn" aria-hidden="true"></i> Человек: <span class="btnDropdownPeople"></span> <span class="btnDropdownPeopleFree"></span></button>
             <div class="dropdown-menu">
                 <div class="dropdown-item"><div class="dropdown-item-people">Дети до 15 лет</div><div class="number"><span class="quont-minus"><i class="fa fa-minus" aria-hidden="true"></i></span><input id="inputPeople16" class="input-calc-exc" type="text" min="0" max="100" value="40"><span class="quont-plus"><i class="fa fa-plus" aria-hidden="true"></i></span>
@@ -28,7 +28,7 @@ echo '<link rel="stylesheet" type="text/css" href="'.plugins_url("vlg_plagin/inc
             </div>
         </div>
     </div>
-    <div class="col" style="text-align: center;">
+    <div class="col">
         <div class="btn-group btn-group-margin">
             <button class="btn btn-secondary btn-lg dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-info fa-icon-btn" aria-hidden="true"></i> Дней: <span class="btnDropdownDay">1</span></button>
             <div class="dropdown-menu">
@@ -42,16 +42,22 @@ echo '<link rel="stylesheet" type="text/css" href="'.plugins_url("vlg_plagin/inc
 
 </div>
 
-<!-- btnDropdownDay #inputDays -->
-
-
-
-
-
+<!-- btnDropdownDay #inputDays  vlg-two__delete vlg-two__move -->
 
 <h2 class="excursion-switch-title">ШАГ 2: Места, которые Вы хотите посетить</h2>
 <div class="vlg-two">
-    <div></div>
+</div>
+<div class="vlg-two__list">
+    <div class="vlg-two__item">
+        <div class="vlg-two__time">1 ч 30 м</div>
+        <div class="vlg-two__bus">Автобус</div>
+        <div class="vlg-two_title">Обзорная экскурсия «Город-герой Волгоград» <div><i class="fa fa-trash fa-red vlg-two__delete"></i> <i class="fa fa-arrows-v vlg-two__move"></i></div></div>
+    </div>
+    <div class="vlg-two__item">
+        <div class="vlg-two__time">0 ч 40 м</div>
+        <div class="vlg-two__bus">Автобус</div>
+        <div class="vlg-two_title">Обзорная экскурсия «Город-герой Волгоград» <div><i class="fa fa-trash fa-red vlg-two__delete"></i> <i class="fa fa-arrows-v vlg-two__move"></i></div></div>
+    </div>
 </div>
 
 <h3>Выберите экскурсии, которые вы хотите посетить</h3>
