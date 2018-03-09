fc2c139804bf96e55a6522403f42116bc064792e
zraziu <zraziu@mail.ru>
2018-03-09 22:40:31 +0300

    удалил старый вариант

--- includes/js/script.js
+++ includes/js/script.js
@@ -31,34 +31,6 @@ $(document).ready(function() {
         }
     });
 
-    /*
-    $("#vlgModal .vlg-add-exc, #vlgModal .vlg-add-musem").toggle(
-        function () {
-            var parentBlock = $(this).closest('.vlg-catalog__item'); // блок  м
-            parentBlock.addClass('vlg-catalog__BlActive'); // стиль блок м
-
-            $(this).find('i').removeClass('fa-plus fa-add-green');
-            $(this).find('i').addClass('fa-times fa-add-red');
-            $(this).find('span').text('Удалить');
-
-            // добавить в список
-            var vlgListExc = '<div id="'+ parentBlock.attr('id') +'list" class="vlg-two__item '+ parentBlock.attr('id') +'"><div class="vlg-two_info"><div class="vlg-two__time">'+ parentBlock.find('.vlg-catalog__hours').text() +'</div><div class="vlg-two__bus">Автобус</div></div><div class="vlg-two_title">'+ parentBlock.find('.vlg-catalog__title').text() +'<div class="vlg-two__btn"><i class="fa fa-trash fa-red vlg-two__delete"></i> <i class="fa fa-arrows vlg-two__move"></i></div></div></div>';
-            $('#vlgListExc').append(vlgListExc);
-            
-        },
-        function () {
-            var parentBlock = $(this).closest('.vlg-catalog__item');
-            parentBlock.removeClass('vlg-catalog__BlActive');
-            $(this).find('i').removeClass('fa-times fa-add-red');
-            $(this).find('i').addClass('fa-plus fa-add-green');
-            $(this).find('span').text('Добавить');
-            // удалить из списка
-            $('#vlgListExc [id ^= "'+ parentBlock.attr('id') +'"]').remove();
-        }
-    );
-    */
-
-
     $('#vlgListExc').on('click', 'i.vlg-two__delete', function(){
         var vlgListItem = $(this).closest('.vlg-two__item'); // нужный блок
         var vlgListItemId = $('#vlgModal #'+vlgListItem.attr("id").split('list')[0]); // нужный блок модал
