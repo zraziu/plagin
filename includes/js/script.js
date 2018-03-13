4c2618d1f1bff13924c2cac462343abd31daa243
zraziu <zraziu@mail.ru>
2018-03-13 09:51:24 +0300

    убрал пробелы, переносы

--- includes/js/script.js
+++ includes/js/script.js
@@ -13,44 +13,32 @@ $(document).ready(function() {
     $("#vlgModal .vlg-add-exc, #vlgModal .vlg-add-musem").click(function () {
         var parentBlock = $(this).closest('.vlg-catalog__item');
 
-        if (parentBlock.is('.vlg-catalog__BlActive')) { // удаление
-
+        if (parentBlock.is('.vlg-catalog__BlActive')) { // удаление в модал
             parentBlock.removeClass('vlg-catalog__BlActive');
             parentBlock.find('i.fa-add-exc').replaceWith('<i class="fa fa-plus fa-add-exc fa-add-green"><span>Добавить</span></i>');
-
             // удалить из списка
             $('#vlgListExc [id ^= "'+ parentBlock.attr('id') +'"]').remove();
-            
             // счетчик
             countExc(parentBlock, 'minus');
-
-        } else {  // добавление
-
-            parentBlock.addClass('vlg-catalog__BlActive'); // стиль блок м
+        } else {  // добавление в модал
+            parentBlock.addClass('vlg-catalog__BlActive'); // добавить стиль
             parentBlock.find('i.fa-add-exc').replaceWith('<i class="fa fa-times fa-add-exc fa-add-red"><span>Удалить</span></i>');
-
             // добавить в список строку
-            var vlgListExc = '<div id="'+ parentBlock.attr('id') +'list" class="vlg-two__item '+ parentBlock.attr('id') +'"><div class="vlg-two_info"><div class="vlg-two__time">'+ parentBlock.find('.vlg-catalog__hours').text() +'</div><div class="vlg-two__bus">Автобус</div></div><div class="vlg-two_title">'+ parentBlock.find('.vlg-catalog__title').text() +'<div class="vlg-two__btn"><i class="fa fa-trash fa-red vlg-two__delete"></i> <i class="fa fa-arrows vlg-two__move"></i></div></div></div>';
+            var vlgListExc = '<div id="'+ parentBlock.attr('id') +'list" class="vlg-two__item '+ parentBlock.attr('id') +'"><div class="vlg-two_info"><div class="vlg-two__time">'+ parentBlock.find('.vlg-catalog__hours span').text() +'</div><div class="vlg-two__bus">Автобус</div></div><div class="vlg-two_title">'+ parentBlock.find('.vlg-catalog__title').text() +'<div class="vlg-two__btn"><i class="fa fa-trash fa-red vlg-two__delete"></i> <i class="fa fa-arrows vlg-two__move"></i></div></div></div>';
             $('#vlgListExc').append(vlgListExc);
-
             // счетчик
             countExc(parentBlock, 'plus');
         }
-
     });
 
-    $('#vlgListExc').on('click', 'i.vlg-two__delete', function(){
+    $('#vlgListExc').on('click', 'i.vlg-two__delete', function(){ // удаление блоков в списке
         var vlgListItem = $(this).closest('.vlg-two__item'); // нужный блок
         var vlgListItemId = $('#vlgModal #'+vlgListItem.attr("id").split('list')[0]); // нужный блок модал
-
         vlgListItem.remove(); // удалить в списке
-
         vlgListItemId.removeClass('vlg-catalog__BlActive'); // удалить в модальном
         vlgListItemId.find('i.fa-add-exc').replaceWith('<i class="fa fa-plus fa-add-exc fa-add-green"><span>Добавить</span></i>');
-
         // счетчик
         countExc(vlgListItemId, 'minus');
-
     });
 
 
